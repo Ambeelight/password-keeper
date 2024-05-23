f64f1f0e04bc36801205f9c70199493e46e3ea1c
Ambeelight <imakar98@gmail.com>
2024-05-23 23:46:22 +0300

    Code improvements

--- backend/controllers/logout.js
+++ backend/controllers/logout.js
@@ -6,14 +6,10 @@ const router = express.Router()
 
 router.delete('/', validSessionToken, async (req, res) => {
 	const token = req.validSessionToken
-	console.log('LogOutToken', token)
 
 	if (token) {
-		console.log('SessionToken', token)
-
 		await token.deleteOne()
 		res.status(200).json({ message: 'You have logged out' })
-		console.log('You have logged out')
 	} else {
 		res.status(401).json({ error: 'token invalid' })
 	}
